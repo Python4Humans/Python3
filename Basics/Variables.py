0e88079240d6c02b0f624dc03ef03930996bd827
Rares Dima <rares.dima96@yahoo.com>
2018-08-17 21:10:16 +0300

    Added spacing for code readability and a newline at the end of the file.

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -4,16 +4,20 @@ A place for all variable data
 
 # A variable is the main data container that all programming languages use.
 # The most common question I always get when people see:
+
 x = 0
 y = 1
 xy = x + y
 yx = y + x
+
 # Is how do you know that x = 0 ...?
 # The magic is we don't, it could have been:
+
 Work = 0
 Sucks = 1
 WorkSucks = Work + Sucks
 SucksWork = Sucks + Work
+
 # Presto! We name our own variables whatever we choose.
 # No one will ever be able to care unless they see the code base.
 # Variable names do not show in production / execution. Only their values.
@@ -30,6 +34,8 @@ none = None
 # Variables containing multiple words are usually written using snakecase (separate words by an underscore).
 # It's good practice to uppercase all variables that will stay the same throughout execution.
 # Example:
+
 PROGRAM_NAME = 'Variables.py'
+
 # In the functions area [os,sys] both have unique ways of grabbing this file's name. 
 # This becomes useful incase it is changed by you, a user.
