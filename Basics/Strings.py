5b33a251ee927aafe7e03d3027a46777baf96d03
Rares Dima <rares.dima96@yahoo.com>
2018-08-17 21:26:05 +0300

    Removed title case for all comments in the Strings module.
    Title case in regular text is not grammatically correct in English.

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -2,62 +2,62 @@
 This Is The Strings Section.
 '''
 
-# The Objects Holding The Strings Are From The [Variable] Section
-# If You Do NOT Understand What A Variable Is Please Head Over To Basics/Variables.py Then Return.
-# A String Is Anything Interpreted To Be Read.
+# The objects holding the Strings are from the [Variable] section.
+# If you do NOT understand what a variable is please head over to basics/Variables.py then return.
+# A String is anything interpreted to be read.
 STRING_CASE_ONE = ''
 STRING_CASE_TWO = ""
-# You May Use Either At Any Point In Time.
+# You may use either at any point in time.
 
 # Examples:
 my_string_one = 'this is a string'
 my_string_two = "this also is a string"
 
-# Something You Will Notice That Will NOT Work: 'This String Is Skrypt's'
-# Correction Below:
+# Something you will notice that will NOT work: 'This String Is Skrypt's'.
+# Correction below:
 example_0 = 'This String Is Skrypt\'s'
-# Notice The [\] Which Escapes our First ['] And Tells Python It Is Now Apart Of The Expression Not Statement.
-# This Also Works
-example_0a = "This String Is Skrypt's" # Notice No [\] Needed
-# Using ["] We Would Need [\] While Doing Something Like This
+# Notice the [\] which escapes our first ['] and tells Python it is now a part of the expression not statement.
+# This also works:
+example_0a = "This String Is Skrypt's" # Notice no [\] needed
+# Using ["] we would need [\] while doing something like this:
 example_0b = "Vivian Reads \"Strings\" As The Title Of This Script"
-# Or Alternativly
+# Or alternatively:
 example_0ba = 'Vivian Reads "Strings" As The Title Of This Script'
-# Above Output Would Be "Vivian Reads "Strings" As The Title Of This Script"
+# The above output would be "Vivian Reads "Strings" As The Title Of This Script"
 
-# There Are A Few Neat Things Python Is Able To Do With Strings.
-# Below Are Examples That Use Items From The [Functions,KeyWords, Operators] Sections
-# If You Get Confused Jump Right Over There And Take A Look And Come Back.
+# There are a few neat things Python is able to do with Strings.
+# Below are examples that use items from the [Functions, KeyWords, Operators] sections.
+# If you get confused jump right over there and take a look and come back.
 
-# One Of The Most Common Ways To Use A String Is To print() A Message To The Console.
+# One of the most common ways to use a String is to print() a message to the console.
 print('this is what appears on your console')
-# First Usefule Item For Strings Is len() Used To Get The Length Of A String Object (Other Objects Too)
-# Normally Python Would Start At 0 With Counting But This Isn't The Case Here
-example_1 = len(my_string_one) # Output: 16 (Notice Python Also Does Not Count The ['] String Brackets)
-# Strings Have Positional Memory That Python Can Choose With A Integer.
+# First useful item for Strings is len(), used to get the length of a String object (Other objects too).
+# Normally Python would start at 0 with counting but this isn't the case here.
+example_1 = len(my_string_one) # Output: 16 (Notice Python also does not count the ['] String brackets)
+# Strings have positional memory that Python can choose with an Integer.
 example_1a = my_string_one[0] # Output: 't'
-# You May Also Ask Python For A Portion Of A String Using [:]
+# You may also ask Python for a portion of a String using [:].
 # Example [INT:]
-# Below Will Return The String Starting At The 1 Position 'his is a string'
+# The below example will return the String atarting at the 1 Position 'his is a string'.
 example_1b = my_string_one[1:]
-# Below Will Return The Portion Starting At The 2 postion Ending At The 4 Position
-# Notice How We Collect At Position 2 But Drop At Position 3, 4 Is Left Out.
+# The below example will return the portion starting at the 2 postion and ending at the 4 position.
+# Notice how we collect at position 2 but drop at Position 3, 4 is left out.
 example_1c = my_string_one[2:4] # Output: 'is'
-# To Reverse A String The Pythonic Term Is [::-1] On The End Of Any String Expression
+# To reverse a String the Pythonic term is [::-1] on the end of any String expression.
 example_2 = 'normal string'
 example_2a = example_2[::-1] # Output: 'gnirts lamron'
 example_2b = example_2[2:5][::-1] # Output: 'amr'
 
-# Strings Can Be Added Together Or To With The '+' Operator
+# Strings can be added together with the '+' operator.
 example_3 = ''
 example_3a = 'Hello'
 example_3b = ' ' # Space
 example_3c = 'World'
 example_3d = example_3a+example_3b+example_3c # Output: 'Hello World'
-# Alternativly
+# Alternatively:
 example_3e = 'Hello'+' '+example_3c # Output: 'Hello World'
 example_3 += example_3a[0] # Output: example_3 = 'H'
-# Python Has Built In Methods For Strings Like .upper(), .lower(), .title()
+# Python has builtin methods for Strings like .upper(), .lower(), .title().
 example_4 = 'our string'
 example_4.upper() # Output: 'OUR STRING'
 example_4.lower() # Output: 'our string'
